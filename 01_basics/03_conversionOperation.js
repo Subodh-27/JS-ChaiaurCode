ea7b43d1396bf9c1f3beace7676583639bded894
Subodh Awate <102420150+Subodh-27@users.noreply.github.com>
2023-09-22 18:29:53 +0000

    Datatype conversions

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,31 @@
+/* let score = "33abc"
+
+console.log(typeof(score));
+
+let valueInNumber = Number(score);
+console.log(valueInNumber);
+console.log(typeof(valueInNumber));
+
+let value1 = null
+let convertedNumValue = Number(value1);
+console.log(convertedNumValue);
+console.log(typeof(convertedNumValue));
+
+let value2 = undefined;
+let convertedNumValue1 = Number(value2);
+console.log(convertedNumValue1);
+console.log(typeof(convertedNumValue1));
+
+let value3 = true;
+let convertedNumValue3 = Number(value3);
+console.log(convertedNumValue3);
+console.log(typeof(convertedNumValue3)); */
+
+/* let isLoggedIn = 'A';
+let checkLogin = Boolean(isLoggedIn);
+console.log(checkLogin); */
+
+/* let num1 = 22
+let num1ToString = String(num1)
+console.log(num1ToString);
+console.log(typeof num1ToString); */
